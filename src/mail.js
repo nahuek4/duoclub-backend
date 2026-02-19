7b201b1964058b8681191e2812626401f09c9bb8
nahuek4 <nahuek.75@gmail.com>
2026-02-18 23:19:43 -0300

    test

---
+++ src/mail.js
@@ -0,0 +1,7 @@
+// backend/src/mail.js
+export * from "../duoclub-backend/src/mail/core.js";
+export * from "../duoclub-backend/src/mail/authEmails.js";
+export * from "../duoclub-backend/src/mail/appointmentEmails.js";
+export * from "../duoclub-backend/src/mail/admissionEmails.js";
+export * from "../duoclub-backend/src/mail/orderEmails.js";
+export * from "../duoclub-backend/src/mail/layout.js";
