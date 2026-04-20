8200360504458c0f2cf9c024b360f2b614288fa3
nahuek4 <nahuek.75@gmail.com>
2026-04-20 06:16:53 -0300

    test

--- src/mail.js
+++ src/mail.js
@@ -1,4 +1,6 @@
 // backend/src/mail.js
+// Entry point único para centralizar exports y mantener compatibilidad
+// con nombres viejos usados por algunas rutas.
 
 export * from "./mail/core.js";
 export * from "./mail/authEmails.js";
@@ -7,20 +9,30 @@ export * from "./mail/admissionEmails.js";
 export * from "./mail/orderEmails.js";
 export * from "./mail/layout.js";
 export * from "./mail/creditsEmails.js";
+export * from "./mail/helpers.js";
+export * from "./mail/ui.js";
 
-/* =========================================================
-   FIX COMPATIBILIDAD
-========================================================= */
+import {
+  sendAdminOrderPendingEmail,
+  sendOrderPendingEmail,
+  sendOrderPaidEmail,
+  sendOrderCancelledEmail,
+} from "./mail/orderEmails.js";
 
-// CREDITOS
-export {
-  sendAdminCreditsChangedEmail as sendAdminCreditsAssignedEmail,
-  sendCreditsChangedEmail as sendUserCreditsAssignedEmail,
+import {
+  sendCreditsChangedEmail,
+  sendAdminCreditsChangedEmail,
 } from "./mail/creditsEmails.js";
 
-// ORDERS
+// Aliases legacy / compatibilidad
 export {
   sendAdminOrderPendingEmail as sendAdminNewOrderEmail,
+  sendAdminOrderPendingEmail as sendAdminOrderEmail,
   sendOrderPendingEmail as sendUserOrderCashCreatedEmail,
+  sendOrderPendingEmail as sendOrderCashCreatedEmail,
   sendOrderPaidEmail as sendUserOrderPaidEmail,
-} from "./mail/orderEmails.js";
+  sendOrderCancelledEmail as sendUserOrderCancelledEmail,
+  sendCreditsChangedEmail as sendUserCreditsAssignedEmail,
+  sendAdminCreditsChangedEmail as sendAdminCreditsAssignedEmail,
+  sendCreditsChangedEmail as sendCreditsAssignedEmail,
+};
